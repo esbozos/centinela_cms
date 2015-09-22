582fca0fc44c6af7099f8eb0a0a59b6394a4222f
Norman Torres <norman.nets@gmail.com>
2015-09-22 00:05:25 -0430

    set news as index, and index as about prospectiva...

--- centinela/urls.py
+++ centinela/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^noticias/(?P<slug>[a-zA-Z0-9-]+)/(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^noticias/$', views.NewsView.as_view(), name='news'),
-    url(r'^noticias/(?P<category>[a-zA-Z0-9-]+)/$', views.CategoryPostList.as_view(), name='category_post_list'),
+    url(r'^nosotros/$', views.IndexView.as_view(), name='index'),
+    url(r'^/(?P<slug>[a-zA-Z0-9-]+)/(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.NewsView.as_view(), name='news'),
+    url(r'^(?P<category>[a-zA-Z0-9-]+)/$', views.CategoryPostList.as_view(), name='category_post_list'),
     url(r'^(?P<category>[a-zA-Z0-9-]+)/(?P<slug>[a-zA-Z0-9-]+)/(?P<post_id>[0-9]+)/$', views.page, name='page')
 ]
 
