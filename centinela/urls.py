2def4dce04378af57cabf5a49ffbbf567cdad1e7
Norman Torres <norman.nets@gmail.com>
2015-09-22 09:49:20 -0430

    fixed bug url to show post detail

--- centinela/urls.py
+++ centinela/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
 
     url(r'^nosotros/$', views.IndexView.as_view(), name='index'),
-    url(r'^/(?P<slug>[a-zA-Z0-9-]+)/(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<slug>[a-zA-Z0-9-]+)/(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^$', views.NewsView.as_view(), name='news'),
     url(r'^(?P<category>[a-zA-Z0-9-]+)/$', views.CategoryPostList.as_view(), name='category_post_list'),
     url(r'^(?P<category>[a-zA-Z0-9-]+)/(?P<slug>[a-zA-Z0-9-]+)/(?P<post_id>[0-9]+)/$', views.page, name='page')
