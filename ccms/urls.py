faa0a47280824775ef2d14b65f77d30533853e00
Norman Torres <norman.nets@gmail.com>
2015-09-22 00:08:24 -0430

    fixed url admin/ to security/admin

--- ccms/urls.py
+++ ccms/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'', include('centinela.urls', namespace="centinela")),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^security/admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
