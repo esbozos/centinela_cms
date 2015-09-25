5fe732dbdc4d5b88dba2b4f305981085167a3abf
Norman Torres <norman.nets@gmail.com>
2015-09-25 09:09:48 -0430

    loading only required js and combine resources

--- static/js/main.js
+++ static/js/main.js
@@ -20,9 +20,6 @@ $(document).ready(function() {
             $(this).hide();
         });
 
-
-
-
     $('.news_list_block .content').find('img').each(function(){
             newsImage = $('<img>');
             newsImage.attr('src', $(this).attr('src'));
@@ -61,3 +58,60 @@ function resize(){
     $('#carousel .item').css({height: ch + 'px'});
     $('.wraper').css({minHeight: wr + 'px'});
 };
+
+
+jQuery(document).ready(function ($) {
+
+    var slideCount = $('#banner ul li').length;
+	var slideWidth = $('#banner ul li').width();
+	var slideHeight = $('#banner ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+
+	if (slideCount > 1){
+	$('#banner li').hide();
+	$('#banner li.active').show();
+   setInterval(function () {
+        moveRight();
+    }, 5000);
+    }
+
+
+
+
+	$('#banner').css({ width: '100%', maxHeight: '250px' });
+
+    $('#banner ul li:last-child').prependTo('#banner ul');
+
+    function moveLeft() {
+        $('#banner ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#banner ul li:last-child').prependTo('#banner ul');
+            $('#banner ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+
+        $('#banner ul').animate({
+            left: - slideWidth
+        }, 800, function () {
+            var li = $('#banner ul li:first-child').appendTo('#banner ul');
+            $('#banner li.active').removeClass('active');
+            li.addClass('active');
+            li.show("slow");
+            li.siblings().hide();
+            $('#banner ul').css('left', '');
+
+         });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+
+});
