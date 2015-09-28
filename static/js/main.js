d07449b9028d31d1203094f0b7339521207d6ea3
Norman Torres <norman.nets@gmail.com>
2015-09-28 13:45:38 -0430

    beta 3.0 thumbnails fixed bug on save post when this don't have a image in content body...

--- static/js/main.js
+++ static/js/main.js
@@ -119,3 +119,16 @@ jQuery(document).ready(function ($) {
     });
 
 });
+
+$(document).ready(function() {
+   $('.content').find('img').each(function(){
+        var max = $(this).parents('.content').width();
+        var oldStyle = $(this).attr('style');
+        $(this).attr('style', oldStyle + '; height: auto; margin: 10px;');
+        $(this).addClass('img img-responsive img-thumbnail');
+    });
+
+    $('table').each(function(){
+        $(this).addClass('table table-bordered table-striped table-hover');
+    });
+});
