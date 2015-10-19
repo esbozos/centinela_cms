1e43b6969c1a18cb92a0ce65024e1869b711506b
Norman Torres <norman.nets@gmail.com>
2015-10-19 16:54:12 -0430

    beta 5 better code for banner

--- centinela/static/js/main.js
+++ centinela/static/js/main.js
@@ -66,25 +66,20 @@ function resize(){
 
 jQuery(document).ready(function ($) {
 
-    var slideCount = $('#banner ul li').length;
-	var slideWidth = $('#banner ul li').width();
-	var slideHeight = $('#banner ul li').height();
-	var sliderUlWidth = slideCount * slideWidth;
 
+    var slideCount = $('#banner-content div').length;
 	if (slideCount > 1){
-	$('#banner li').hide();
-	$('#banner li.active').show();
+	$('.banner-item').hide();
+	$('.banner-item.active').show();
    setInterval(function () {
         moveRight();
     }, 5000);
     }
 
 
-
-
 	$('#banner').css({ width: '100%', maxHeight: '250px' });
 
-    $('#banner ul li:last-child').prependTo('#banner ul');
+    $('#banner-content.banner-item:last').prependTo('#banner-content');
 
     function moveLeft() {
         $('#banner ul').animate({
@@ -96,17 +91,18 @@ jQuery(document).ready(function ($) {
     };
 
     function moveRight() {
-
-        $('#banner ul').animate({
+        var slideWidth = $('#banner').width();
+        var slideHeight = $('#banner').height();
+        $('.banner-item.active').animate({
             left: - slideWidth
         }, 800, function () {
-            var li = $('#banner ul li:first-child').appendTo('#banner ul');
-            $('#banner li.active').removeClass('active');
-            li.addClass('active');
-            li.animate({"left":"0px"}, "slow");
-            li.siblings().hide();
-            $('#banner ul').css('left', '');
+            var div = $('#banner-content div:first-child');
+            div.appendTo('#banner-content');
 
+            $('.banner-item.active').removeClass('active');
+            div.siblings().hide();
+            div.addClass('active');
+            div.show();
          });
     };
 
